7b10ac4bd2cd5d93d365a59c47048fed60fee3b1
Joseph Kregloh <5833499+funzie19@users.noreply.github.com>
2020-01-21 17:07:57 -0500

    PENG-14437 - Update metadata.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,10 @@ maintainer_email  'paas-integration@gannett.com'
 license           ' Copyright (c) 2017 Gannett Co., Inc, All Rights Reserved.'
 description       'Installs Python, pip and virtualenv. \
                    Includes LWRPs for managing Python packages with `pip` and `virtualenv` isolated Python environments.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 version           '2.0.3'
+
 chef_version '>= 12.11.18'
 
 depends           'build-essential'
@@ -14,6 +17,6 @@ depends           'yum-gd'
 source_url 'https://github.com/GannettDigital/chef-python'
 issues_url 'https://github.com/GannettDigital/chef-python/issues'
 
-%w(centos redhat).each do |os|
+%w(centos).each do |os|
   supports os
 end
