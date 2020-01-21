b4505fae115cfabd248403424f356f39e9ba9178
Joseph Kregloh <5833499+funzie19@users.noreply.github.com>
2020-01-21 17:40:39 -0500

    PENG-14437 - Fixed spacing issue.

--- metadata.rb
+++ metadata.rb
@@ -1,18 +1,18 @@
-name              'python'
-maintainer        'PAAS'
-maintainer_email  'paas-integration@gannett.com'
-license           ' Copyright (c) 2017 Gannett Co., Inc, All Rights Reserved.'
-description       'Installs Python, pip and virtualenv. \
-                   Includes LWRPs for managing Python packages with `pip` and `virtualenv` isolated Python environments.'
+name 'python'
+maintainer 'PAAS'
+maintainer_email 'paas-integration@gannett.com'
+license ' Copyright (c) 2017 Gannett Co., Inc, All Rights Reserved.'
+description 'Installs Python, pip and virtualenv. \
+            Includes LWRPs for managing Python packages with `pip` and `virtualenv` isolated Python environments.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version           '2.0.3'
+version '2.0.3'
 
 chef_version '>= 12.11.18'
 
-depends           'build-essential'
-depends           'yum-epel'
-depends           'yum-gd'
+depends 'build-essential'
+depends 'yum-epel'
+depends 'yum-gd'
 
 source_url 'https://github.com/GannettDigital/chef-python'
 issues_url 'https://github.com/GannettDigital/chef-python/issues'
